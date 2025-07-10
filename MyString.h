6be7e7e25277f2c634c8c54c038a5666562f589d
grukall <dobana12345@naver.com>
2025-07-10 16:41:52 +0900

    MyStringClass init

---
+++ MyString.h
@@ -0,0 +1,44 @@
+class MyString
+{
+    char *string_content;
+    int string_length;
+    int memory_capacity;
+
+    private:
+    protected:
+    public:
+        MyString(char c);
+        MyString(const char* str);
+
+        //복사 생성자
+        MyString(const MyString &str);
+        ~MyString();
+
+        //길이를 리턴한다.
+        int length() const;
+        //문자열 출력
+        void print() const;
+        //문자열 출력후 개행
+        void println() const;
+
+        MyString &assign(const char *str);
+        MyString &assign(const MyString &str);
+
+        int capacity() const;
+        void reserve(int size);
+        char at(int i) const;
+
+        MyString &insert(int loc, const MyString& str);
+        MyString &insert(int loc, const char* str);
+        MyString &insert(int loc, char c);
+
+        MyString &erase(int loc, int num);
+
+        int find(int find_from, const MyString& str) const;
+        int find(int find_from, const char* str) const;
+        int find(int find_from, char c) const;
+
+        int compare(const MyString& str);
+
+};
+
